f936227f1409a86a58d8b9c3c901a692cda27ad2
Matt Black <mjblack23@Gmail.com>
2017-06-10 21:39:52 -0500

    'auto-commit'

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,8 @@ const ReactDOM = require('react-dom');
 const { Route, Router, IndexRoute, hashHistory, browserHistory } = require('react-router');
 
 const Main = require('Main');
+const Timer = require('Timer');
+const Countdown = require('Countdown');
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
@@ -14,7 +16,8 @@ require('style!css!sass!applicationStyles');
 ReactDOM.render(
   <Router history={ hashHistory }>
     <Route path="/" component={ Main }>
-      
+      <Route path='countdown' component={ Countdown } />
+      <IndexRoute component={ Timer } />
     </Route>
   </Router>,
   document.getElementById('app')
