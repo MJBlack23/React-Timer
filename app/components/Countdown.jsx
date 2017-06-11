17d4f3c8ce4340a1c6b303e29412768b02524da5
Matt Black <mjblack23@Gmail.com>
2017-06-11 17:05:41 -0500

    'auto-commit'

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
 const Clock = require('Clock');
 const CountdownForm = require('CountdownForm');
+const Controls = require('Controls');
 
 const Countdown = React.createClass({
   getInitialState: function () {
@@ -15,7 +16,17 @@ const Countdown = React.createClass({
         case 'started':
           this.startTimer();
 
-        break;
+          break;
+        case 'stopped':
+          this.setState({
+            count: 0
+          });
+
+        case 'paused':
+          clearInterval(this.timer);
+          this.timer = undefined;
+          break;
+
       }
     }
   },
@@ -33,12 +44,26 @@ const Countdown = React.createClass({
       countdownStatus: 'started'
     });
   },
+  handleStatusChange: function (newStatus) {
+    this.setState({
+      countdownStatus: newStatus
+    });
+  },
   render: function () {
-    const { count } = this.state;
+    const { count, countdownStatus } = this.state;
+
+    const renderControlArea = () => {
+      if (countdownStatus !== 'stopped') {
+        return <Controls countdownStatus={ countdownStatus } onStatusChange={ this.handleStatusChange }/>
+      } else {
+        return <CountdownForm onSetCountdown={ this.handleSetCountdown }/>
+      }
+    }; // end renderControlArea
+
     return (
       <div>
         <Clock totalSeconds={ count } />
-        <CountdownForm onSetCountdown={ this.handleSetCountdown }/>
+        { renderControlArea() }
       </div>
     )
   }
